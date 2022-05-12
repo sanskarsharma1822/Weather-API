4e4a9d6e454ab69757d9864cfea7de060ed8478f
sanskarsharma1822 <76549925+sanskarsharma1822@users.noreply.github.com>
2022-05-12 19:51:00 +0530

    Removed unnecessary code from app.js

--- app.js
+++ app.js
@@ -42,19 +42,4 @@ app.listen('3000',function(){
 
 
 
-// var url = "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=c798ed3fd96c02090d64111808046c45&units=metric" ;
-// https.get(url,function(response){
-//   console.log(response.statusCode);
-//
-//   response.on("data",function(data){
-//     const weatherData = JSON.parse(data);
-//     const temp = weatherData.main.temp;
-//     const weatherDescription = weatherData.weather[0].description;
-//     const icon = weatherData.weather[0].icon;
-//     const iconImage = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
-//     res.write("<p>The weather conditon is "+weatherDescription+"</p>");
-//     res.write("<h1>The temparature in Delhi is "+temp+" degress Celcius</h1>");
-//     res.write("<img src = "+iconImage+">");
-//     res.send();
-//   })
-// });
+
